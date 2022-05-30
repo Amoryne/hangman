263ba7b509d3162ea27f445860d10572df7bda63
Amoryne <amoryne.jolimont@gmail.com>
2022-05-30 17:53:54 +0200

    wip

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,25 @@
 //Hangam Game
-
-// Faire une liste de mots V
-// Choisir des mots randoms v
-// Cacher le mot
-// Remplacer la taille du mot par des _
-// Afficher un alphabet 
-// Alphabet cliquable et choisir les lettres
-// Quand on clique, ça recherche si la lettre fait partie du mot Random caché
-// Si oui, elle s'affiche et remplace le _
-// Si Non, on perd une vie(image, chiffres,icone...)
-// Un compteur se lance pour décompter le nombre de vie ( max 5)
-// Quand on arrive a 0=> game over => un message apparait pour dire de cliquer sur Reset pour recommencer
+//**TODO */
+////Faire une liste de mots V
+// ///Choisir des mots randoms v
+// //Cacher le mot
+/////Remplacer la taille du mot par des _
+//// Afficher un alphabet 
+//// Alphabet cliquable et choisir les lettres
+//// Quand on clique, ça recherche si la lettre fait partie du mot Random caché
+//// Si oui, elle s'affiche et remplace le _
+//// Si Non, on perd une vie(image, chiffres,icone...)
+//// Un compteur se lance pour décompter le nombre de vie ( max 5)
+//// Quand on arrive a 0=> game over => un message apparait pour dire de cliquer sur Reset pour recommencer
 // On relance le code et le jeu se remet a 0
+// Faire une fonction reset pour les error
+// Afficher le mot une fois qu'on a perdu
+//Cacher les boutons après le gameover
 // Si on gagne, le mot s'affiche en entier => Message => Inviter a reset pour recommencer
 // Il y a un compteur de victoire et de défaites(optionel)
+//RANDOM SUR LE NOMBRE DE VIES
 //---------------------------------------------------------------
+
 const indice = [
   "Série télévisée",
   "Jeux vidéo médiéval",
@@ -39,30 +44,46 @@ const words = [
   "beach",
   "redbull",
 ];
+const explication = [
+  "Série créer par AA",
+  "CD PROJECT",
+  "Rongeur bruyant",
+  "Crash landing d'amour",
+  "Meilleure pays du monde",
+  "Meilleure animal au monde",
+  "Meilleure ville au monde",
+  "Une européene super chère",
+  "c'est la plage quoi",
+  "Bien meilleure que le Monster",
+];
+
+//MOT ET INDICE RANDOM
+
 let randomWord=words[Math.floor(Math.random()*words.length)];
-// let arrayWord=[randomWord];
 let arrayWord=randomWord.split("");
-let indiceDOM=document.getElementById("indice")
+let indiceDOM=document.getElementById("indice");
+let lives=document.getElementById("lives");
+let details=document.getElementById("explication")
 
-// let wordIndex=words.indexOf(randomWord);
-// let indiceIndex=indice.indexOf(indice)
-// console.log(indiceIndex)
-// console.log(wordIndex);
+
+
+//REGLES DU JEU
+
+
+var maxError = Math.floor(Math.random()*(10 - 2)) + 2;
+lives.innerHTML=" Vous avez pour cette manche: " +maxError + " vies";
+var good=0;
+var error=maxError;
+console.log(maxError)
+// function getRandomLives(){
+//   let maxErrorRandom=
+
+// }
 console.log(arrayWord)
-
-var error=0;
-
-const maxError = 5;
-let good=0;
-
 //---------------------------------------------------------------------------//
 
-//GAME INIT POUR REFRESH-------------------
-// function init(){
-// let lives=5;
-// }
-// init();
-//----------------------------------------//
+
+
 //AFFICHER INDICE LIE AU MOT
 //afficher valeur où indice dont la position est égale a l'index de randomword//
 let indexWord= words.indexOf(randomWord);
@@ -71,19 +92,23 @@ let indicePos=indice.at(indexWord)
 indiceDOM.innerHTML=indicePos;
 console.log(indicePos)
 
-//AFFICHER LE MOT RANDOM-------------------
+
+
+
+
+
+
+
+//AFFICHER LE MOT CACHER "_"-------------------
 let FindWord=document.getElementById("find-word");
-
-
 let hiddenWord = [];
 for(let i = 0; i < randomWord.length; i++){
   hiddenWord[i]= '_';
-//FindWord.innerHTML = hiddenWord;
+
 let p= document.createElement('span');
   p.setAttribute('class','hiddenword');
   p.innerHTML = hiddenWord[i];
   FindWord.appendChild(p);
-  
 
 }
 
@@ -112,7 +137,7 @@ alphabet.forEach((letter) => {
 //AFFICHER LETTRE CHOISIE----------------------------
 let guessWord = document.getElementById("guess-word");
 //VAR BOUTON
-let buttons = document.querySelectorAll(".alphaBtn");
+var buttons = document.querySelectorAll(".alphaBtn");
 
 function displayLetter() {
   var finded = false;
@@ -136,11 +161,8 @@ finded=false;
       if (finded !== true) {
         console.log("error")
         finded=false;
-        
-        
-        error++;
+        error--;
         //button.remove();
-        
         updateError();
         
       }
@@ -154,37 +176,98 @@ finded=false;
 
 
 var rejouer = document.getElementById("rejouer")
+//FUNCTION UPDATE ERROR
+
 function updateError(){
-  document.getElementById('loss').innerHTML=error;
-  if(maxError === error){
-    document.getElementById('loss').innerHTML="You loose";
-   
-    var buttonReset = document.createElement('button');
-    buttonReset.setAttribute('onClick',"window.location.reload();")
-    buttonReset.innerHTML="Game over, try again ?"
-    rejouer.appendChild(buttonReset);
-  }
+  document.getElementById('loss').innerHTML="Il vous reste "+ error + " vies";
+  gameOver();
+
 }
+//FUNCTION UPDATE WIN
 function updateWin(){
   document.getElementById('win').innerHTML=good;
+  win();
+  
+}
+
+//FUNCTIO LOOSE -> Affiche message gameover, bouton rejouer et cache les boutons
+function gameOver(){
+   if(error === 0){
+    document.getElementById('loss').innerHTML="You loose";
+    // CREER BOUTON REFRESH
+    // var buttonReset = document.createElement('button');
+    // buttonReset.setAttribute('onClick',"window.location.reload();")
+    // buttonReset.innerHTML="Game over, try again ?"
+    // rejouer.appendChild(buttonReset);
+    //AFFICHER MOT
+    var showAnswer = document.getElementById('answer');
+    showAnswer.innerHTML="The good answer was ";
+    FindWord.innerHTML= randomWord;
+    staySmart();
+    resetError();
+    
+    
+  }
+}
+
+function win(){
   if(good === randomWord.length){
+
     document.getElementById('win').innerHTML="You win";
-   // <button onClick="window.location.reload();">Refresh Page</button>
+  
     var buttonReset = document.createElement('button');
     buttonReset.setAttribute('onClick',"window.location.reload();")
     buttonReset.innerHTML="Rejouer ?"
     rejouer.appendChild(buttonReset);
+    staySmart();
+
   }
 }
 
-function reset(){
-  error=0;
+
+//RESET ERROR
+function resetError(){
+  error=maxError;
+  list.remove();
+  lives.remove();
+}
+//CREER EXPLICATION LIÉ AU MOT
+function staySmart(){
+let indexDetails=indexWord;
+let indexDetailsPos=explication.at(indexWord)
+details.innerHTML=indexDetailsPos;
+console.log(indexDetailsPos)
 }
 
+// function init(){
+//   //init error
+//   error=maxError;
+// //init vies
+//   good=0;
+//   lives;
+//   indicePos;
 
-reset()
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+//DECLARATION DE FONCTIONS----------------
+
+
 updateError();
-updateWin()
+updateWin();
+gameOver();
+win();
 displayLetter();
 
 
@@ -213,6 +296,11 @@ displayLetter();
 // }
 // let index=randomWord[''];
 // console.log(index);
+
+// let wordIndex=words.indexOf(randomWord);
+// let indiceIndex=indice.indexOf(indice)
+// console.log(indiceIndex)
+// console.log(wordIndex);
 
 // const searchTerm = displayLetter(button){
 // const indexOfFirst = randomWord.indexOf(searchTerm);
